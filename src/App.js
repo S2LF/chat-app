57d11b4e8eac9251f1b4b0c42930f222bdee4edf
S2LF <sdeuhelf@gmail.com>
2020-11-06 10:35:40 +0100

    WCS React 02

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import Contact from './components/Contact'
+import React from "react";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div>
-      <Contact/>
+      <Contact
+        name="Tracy Gregory"
+        img="https://randomuser.me/api/portraits/men/86.jpg"
+        status={true}
+      />
+      <Contact
+        name="Gabriel Lawson"
+        img="https://randomuser.me/api/portraits/men/58.jpg"
+        status={false}
+      />
+      <Contact
+        name="Gabriel Lawson"
+        img="https://randomuser.me/api/portraits/men/58.jpg"
+        status={true}
+      />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Contact.css";
 
-import './Contact.css'
-
-function Contact(){
-
-    return(
-        <div className="Contact">
-            <img className="avatar" alt="avatar" src="https://randomuser.me/api/portraits/men/86.jpg"/>
-            <div>
-                <h3 className="name">Tracy Gregory</h3>
-                <br/>
-                <div className="status"><i className="status-online"/> <span className="status-text">online</span></div>
-            </div>
+function Contact({ name, img, status }) {
+  return (
+    <div className="Contact">
+      <img className="avatar" alt="avatar" src={img} />
+      <div>
+        <h3 className="name">{name}</h3>
+        <br />
+        <div className="status">
+          <i className={status ? "status-online" : "status-offline"} />
+          <span className="status-text">{status ? "online" : "offline"}</span>
         </div>
-    )
-
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  status: PropTypes.bool.isRequired,
+  img: PropTypes.string.isRequired
+};
+
+export default Contact;
